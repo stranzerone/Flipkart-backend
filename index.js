41980c607e0841d3d7e649867dcfee49f1d95ce2
stranzerone <113999437+stranzerone@users.noreply.github.com>
2023-08-31 17:28:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import  express  from "express";
 import dotenv from "dotenv"
-import Connections from "../server/database/monogdb.js";
+import Connections from "./database/monogdb.js";
 import DefaultData from "./default.js"
 import router from "./routes/route.js"
 import cors from 'cors'
@@ -10,7 +10,7 @@ import bodyParser from "body-parser";
 
 const app = express()
 app.use(bodyParser.json());
-
+ const PORT =process.env.PORT || 5000
 
 app.use( "*", cors({origin: ["http://localhost:3000","http://localhost:3001"], credentials: true }));
 
@@ -27,7 +27,7 @@ app.use('/',router);
 
 
 
-app.listen(5000,()=>{
+app.listen(PORT,()=>{
     console.log("server is running on 5000")
 })
 
