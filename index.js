29b941e99b2b3b4ccd50264def59e6ea17df69e3
stranzerone <113999437+stranzerone@users.noreply.github.com>
2023-09-06 17:50:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import  express  from "express";
 import dotenv from "dotenv"
-import Connections from "../server/database/monogdb.js";
+import Connections from "./database/monogdb.js";
 import DefaultData from "./default.js"
 import router from "./routes/route.js"
 import cors from 'cors'
